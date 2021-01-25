508059e3bddb182af3881dafefcf67fb24efe7cb
vinceloup <77978240+vinceloup@users.noreply.github.com>
2021-01-25 15:58:38 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,60 @@
+import tweepy
+import time
+import random
+
+print('Bot Starting...')
+
+CONSUMER_KEY = '5Z6uD8c20FSc8G2LPLdLQtwcl'
+CONSUMER_SECRET = 'iD7fKSExCX0uMjYSW1WiE3nC7Lbji9oya1linuRnm7KZEqeeng'
+ACCESS_KEY = '1353698031229612034-WXQH1CP1SdZGPbSVSF6NdhSIPlnie3'
+ACCESS_SECRET = '2lQ3R556QQ5F79jr5qwFI2QEkV7xarcoX08ZVsJWBmrmX'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
+
+FILE_NAME = 'last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def get_insults(file_name):
+    f = open(file_name, 'r')
+    insults = f.read().split('\n')
+    return insults
+
+def pick_insult():
+    insults = get_insults('insults.txt')
+    insult = random.choice(insults)
+    return insult
+
+def reply():
+    insult = pick_insult()
+
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    mentions = api.mentions_timeline(last_seen_id)
+
+    for mention in reversed(mentions):
+        print(str(mention.id) + ' - ' + mention.text)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        print('Replying to tweet - ' + str(mention.id) + ' by @' + mention.user.screen_name)
+        api.update_status('@' + mention.user.screen_name + ' ' + insult, in_reply_to_status_id = mention.id)
+        print('Replied - ' + '@' + mention.user.screen_name + ' ' + insult)
+
+print('Bot Started.')
+
+while True:
+    print('Waiting for Tweets...')
+    reply()
+    time.sleep(10)
+    
